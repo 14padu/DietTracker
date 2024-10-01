dd334c8beaeee1025b98b27e375ed0017211ce44
14padu <padmaja.phn@gmail.com>
2024-10-01 05:23:25 +0000

    processing

--- controllers/dietController.js
+++ controllers/dietController.js
@@ -1,51 +1,51 @@
-const BookModel = require('../models/bookModel');
+const DietModel = require('../models/DietModel');
 
-exports.createBook = async (req, res) => {
+exports.createDiet = async (req, res) => {
     try {
-        let singleBook = new BookModel({ title: req.body.title, author: req.body.author });
-        singleBook = await singleBook.save();
-        res.send(singleBook);
+        let singleDiet = new DietModel({ title: req.body.title, author: req.body.author });
+        singleDiet = await singleDiet.save();
+        res.send(singleDiet);
     } catch (err) {
         res.status(400).send(err.message);
     }
 };
 
-exports.getAllBooks = async (req, res) => {
+exports.getAllDiet = async (req, res) => {
     try {
-        const allBooks = await BookModel.find();
-        res.send(allBooks);
+        const allDiet = await DietModel.find();
+        res.send(allDiet);
     } catch (err) {
         res.status(400).send(err.message);
     }
 };
 
-exports.getBookById = async (req, res) => {
+exports.getDietById = async (req, res) => {
     try {
-        const bookById = await BookModel.findById(req.params.id);
-        if (!bookById) return res.status(404).send('Book not found in database');
-        res.send(bookById);
+        const DietById = await DietModel.findById(req.params.id);
+        if (!DietById) return res.status(404).send('Diet not found in database');
+        res.send(DietById);
     } catch (err) {
         res.status(400).send(err.message);
     }
 };
 
-exports.updateBook = async (req, res) => {
+exports.updateDiet = async (req, res) => {
     try {
-        const bookById = await BookModel.findByIdAndUpdate(req.params.id, { title: req.body.title, author: req.body.author }, { new: true });
-        if (!bookById) return res.status(404).send('Book not found in database');
-        res.send(bookById);
-        console.log("Book updated successfully");
+        const DietById = await DietModel.findByIdAndUpdate(req.params.id, { title: req.body.title, author: req.body.author }, { new: true });
+        if (!DietById) return res.status(404).send('Diet not found in database');
+        res.send(DietById);
+        console.log("Diet updated successfully");
     } catch (err) {
         res.status(400).send(err.message);
     }
 };
 
-exports.deleteBook = async (req, res) => {
+exports.deleteDiet= async (req, res) => {
     try {
-        const bookById = await BookModel.findByIdAndDelete(req.params.id);
-        if (!bookById) return res.status(404).send('Book not found in database');
+        const DietById = await DietModel.findByIdAndDelete(req.params.id);
+        if (!DietById) return res.status(404).send('Diet not found in database');
         res.status(204).send();
-        res.send("Book deleted successfully");
+        res.send("Diet deleted successfully");
     } catch (err) {
         res.status(400).send(err.message);
     }
--- routes/dietRoutes.js
+++ routes/dietRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const DietController =require('../controllers/DietController');
+const DietController =require('../controllers/DietController.js');
 
-    router.post('/Diet', DietController.createDiet);
-    router.get('/Diet', DietController.getAllDiet);
-    router.get('/Diet/:id', DietController.getDietById);
-    router.put('/Diet/:id', DietController.updateDiet);
-    router.delete('/Diet/:id', DietController.deleteDiet);
+    router.post('/Diets', DietController.createDiet);
+    router.get('/Diets', DietController.getAllDiet);
+    router.get('/Diets/:id', DietController.getDietById);
+    router.put('/Diets/:id', DietController.updateDiet);
+    router.delete('/Diets/:id', DietController.deleteDiet);
 
 
     module.exports = router;
