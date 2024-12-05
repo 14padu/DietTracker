db956df02a70fba4e0c5854b89c5d7c47f68dfa0
14padu <padmaja.phn@gmail.com>
2024-12-05 19:44:49 +0530

    Update UpdateDiet.js

--- backupclient/src/components/UpdateDiet.js
+++ backupclient/src/components/UpdateDiet.js
@@ -8,15 +8,15 @@ console.log(API_URL)
 const DietEdit = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [patient, setPatient] = useState({ name: '', age: '' ,co_number:'',gender:''});
+  const [Diet, setDiet] = useState({ name: '', age: '' ,co_number:'',gender:''});
 
   useEffect(() => {
-    const fetchPatient = async () => {
+    const fetchDiet= async () => {
       try {
         const response = await axios.get(`${API_URL}/${id}`);
-        setPatient(response.data);
+        setDiet(response.data);
       } catch (error) {
-        console.error('Error fetching patient:', error);
+        console.error('Error fetching Diet:', error);
       }
     };
     fetchPatient();
@@ -24,21 +24,21 @@ const DietEdit = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setPatient({ ...patient, [name]: value });
+    setDiet({ ...Diet, [name]: value });
   };
 
   const handleUpdate = async (e) => {
     e.preventDefault();
     try {
-      await axios.put(`${API_URL}/${id}`, patient);
-      navigate(`/detail/${id}`); // Redirect to person details page after update
+      await axios.put(`${API_URL}/${id}`, Diet);
+      navigate(`/detail/${id}`); // Redirect to Diet details page after update
     } catch (error) {
-      console.error('Error updating patient:', error);
+      console.error('Error updating Diet:', error);
     }
   };
 
   const handleCancel = () => {
-    navigate(`/detail/${id}`); // Navigate back to the person details page
+    navigate(`/detail/${id}`); // Navigate back to the Diet details page
   };
 
   const handleHome = () => {
@@ -47,22 +47,22 @@ const DietEdit = () => {
 
   return (
     <div className="box-container">
-      <h1>Update Patient</h1>
+      <h1>Update Diet</h1>
       <form onSubmit={handleUpdate} className="form-container">
-        <input type="text" name="name" placeholder="Name" value={patient.name} onChange={handleChange}
+        <input type="text" name="name" placeholder="Name" value={Diet.name} onChange={handleChange}
           required className="input-field"/>
 
-        <input type="number" name="age" placeholder="Age" value={patient.age} onChange={handleChange}
+        <input type="number" name="age" placeholder="Age" value={Diet.age} onChange={handleChange}
           required className="input-field"/>
 
-        <input type="number" placeholder="contact-number" value={patient.co_number} onChange={handleChange} required className="input-field" />
+        <input type="number" placeholder="contact-number" value={Diet.co_number} onChange={handleChange} required className="input-field" />
         
-        <select type='select' placeholder="Select Gender" value={patient.gender} onChange={handleChange} required className='input-field'>
+        <select type='select' placeholder="Select Gender" value={Diet.gender} onChange={handleChange} required className='input-field'>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
             <option value="Other">Other</option> </select>
 
-        <div className="person-actions">
+        <div className="Diet-actions">
           <button type="submit" className="btn btn-update">Update</button>
           <button type="button" className="btn btn-cancel" onClick={handleCancel}>Cancel</button>
           <button type="button" className="btn btn-back" onClick={handleHome}>Back to Home</button>
@@ -72,4 +72,4 @@ const DietEdit = () => {
   );
 };
 
-export default PatientEdit;
+export default DietEdit;
